98637c3a21050358566f2da963f77b2fda016b6f
vignesh <vvignesh1011@gmail.com>
2025-08-15 08:49:08 +0530

    nav bar and home page for mobile responciveness

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import "../styles/NavBar.scss";
 import { Link, NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
 
 type Props = {};
 
 function NavBar({}: Props) {
   return (
     <div className="navbar_container">
-      <ul className="menu_item_container">
+      <input type="checkbox" className="hidden" id="menu_toggler" />
+      <div className="menu_icon">
+        <label htmlFor="menu_toggler">
+          <RiMenu3Line size={26} id="menu_open" />
+          <RiCloseLine size={26} id="menu_close" />
+        </label>
+      </div>
+      <ul className="menu_item_container ">
         <NavLink
           to={"/"}
           className={({ isActive }) => (isActive ? "active" : "")}
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/Home.css";
 import { motion } from "framer-motion";
 import { IoCodeDownload } from "react-icons/io5";
@@ -9,9 +9,18 @@ import { MdEmail } from "react-icons/md";
 type Props = {};
 
 function Home({}: Props) {
+  const resumeRef = useRef<HTMLAnchorElement>(null);
+  const downloadResume = () => resumeRef.current?.click();
+
   return (
-    <div className="flex justify-between items-center  home-container">
+    <div className="flex justify-between items-center  home-container flex-wrap-reverse">
       <div className="flex-1 p-4">
+        <a
+          href="/vignesh's_resume.pdf"
+          className="hidden"
+          ref={resumeRef}
+          download
+        ></a>
         <p>
           <TypeAnimation
             sequence={[
@@ -38,28 +47,49 @@ function Home({}: Props) {
 
         {/* cv and social media links */}
         <div className="home_btn_container">
-          <div className="btn flex items-center ">
+          <div className="btn flex items-center " onClick={downloadResume}>
             Download CV
             <IoCodeDownload className="ml-4 " size={24} />
           </div>
 
           <div className="flex items-center">
-            <div className="btn-round">
+            <div
+              className="btn-round"
+              onClick={() =>
+                window.open("https://github.com/vvignesh1011", "_blank")
+              }
+            >
               <FaGithub />
             </div>
-            <div className="btn-round">
+            <div
+              className="btn-round"
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/vignesh-v-28b31b244",
+                  "_blank"
+                )
+              }
+            >
               <FaLinkedinIn />
             </div>
-            <div className="btn-round">
+            <div
+              className="btn-round"
+              onClick={() =>
+                window.open("mailto:vvignesh1011@gmail.com", "_blank")
+              }
+            >
               <MdEmail />
             </div>
-            <div className="btn-round">
+            <div
+              className="btn-round"
+              onClick={() => window.open("tel:+917708783714", "_blank")}
+            >
               <FaPhoneAlt />
             </div>
           </div>
         </div>
       </div>
-      <div>
+      <div className="mx-auto">
         <div className=" relative flex w-fit ml-auto">
           <img
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
